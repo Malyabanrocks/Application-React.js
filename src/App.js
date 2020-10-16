660f7ef444e0fbf909e6b45025b434fdc76150e3
Malyabanrocks <malyabansingha1993@gmail.com>
2020-10-16 23:59:24 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./App.css";
+import Login from './components/auth/login'
+
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+         
+         <Login/>
+      </div>
+    );
+  }
+}
+
+export default App;
